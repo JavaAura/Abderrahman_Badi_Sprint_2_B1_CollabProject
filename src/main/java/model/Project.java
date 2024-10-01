23247ed066df7f0ccd7c2c8f0a4300516c044627
WALIDSAIFI <saifiwalid21@gmail.com>
2024-10-01 17:07:56 +0100

    SB-14 : ajouter getter setter

--- src/main/java/model/Project.java
+++ src/main/java/model/Project.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Project {
+import model.enums.ProjectStatus;
 
+import java.time.LocalDate;
+import java.util.*;
+
+
+public class Project {
+	private int id;
+    private String nom;
+    private String description;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private ProjectStatus statut;
+    private List<Task> taches;
+
+    public Project(String nom, String description, LocalDate dateDebut, LocalDate dateFin, ProjectStatus statut, List<Task> taches) {
+        this.nom = nom;
+        this.description = description;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+        this.taches = taches;
+    }
+
+    public Project(int id, String nom, String description, LocalDate dateDebut, LocalDate dateFin, ProjectStatus statut, List<Task> taches) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.statut = statut;
+        this.taches = taches;
+    }
+
+
+    
 }
