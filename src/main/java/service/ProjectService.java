e6ecd341daada585af1f1be2e4fb33dbcfd8c22d
WALIDSAIFI <saifiwalid21@gmail.com>
2024-10-01 22:20:26 +0100

    SB-14 : ajouter Projectservice

--- src/main/java/service/ProjectService.java
+++ src/main/java/service/ProjectService.java
@@ -1,5 +1,19 @@
 package service;
 
+import model.Project;
+import repository.interfaces.ProjectRepository;
+
+import java.util.List;
+
 public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.getAllProjects();
+    }
 
 }
