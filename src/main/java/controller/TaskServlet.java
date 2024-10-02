de8c0d17789a9cd7a84a09b3570c2fc5e125c488
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-02 10:12:01 +0100

    SB-18 logger added

--- src/main/java/controller/TaskServlet.java
+++ src/main/java/controller/TaskServlet.java
@@ -2,6 +2,9 @@ package controller;
 
 import java.io.IOException;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TaskServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(TaskServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("views/squads.jsp");
+        logger.info("accessed tasks page");
+        logger.warn("accessed tasks page");
+    
+        RequestDispatcher dispatcher = req.getRequestDispatcher("views/tasks.jsp");
         dispatcher.forward(req, resp);
     }
 
