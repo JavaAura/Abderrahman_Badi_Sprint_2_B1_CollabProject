f48795b9ff50b500202f3099ab310da3c447279d
chaabat <chaabat.02@gmail.com>
2024-10-02 11:25:01 +0100

    SB-23 SquadRepositoryImp getSquadById

--- src/main/java/repository/implementation/SquadRepositoryImpl.java
+++ src/main/java/repository/implementation/SquadRepositoryImpl.java
@@ -14,6 +14,7 @@ import repository.interfaces.SquadRepository;
 public class SquadRepositoryImpl implements SquadRepository {
 	
 	private static final String GET_ALL_SQUADS_QUERY = "SELECT * FROM squad LIMIT ? OFFSET ? " ;
+	private static final String GET_SQUAD_BY_ID_QUERY = "SELECT * FROM squad WHERE id = ?";
 	
 	@Override
 	public List<Squad> getAllSquads(int page , int pageSize){
@@ -44,21 +45,28 @@ public class SquadRepositoryImpl implements SquadRepository {
 		return squads;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+ 
 	@Override
 	public Squad getSquadById(long id) {
-		return null ;
+		Squad squad = null;
+		
+		try(Connection connection = DatabaseConnection.getConnection();
+				PreparedStatement stmt = connection.prepareStatement(GET_SQUAD_BY_ID_QUERY)){
+			
+			stmt.setLong(1,id);
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				squad = new Squad();
+				squad.setId(rs.getLong("id"));
+				squad.setName(rs.getString("name"));
+			}
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		return squad;
 		
 	}
 	
