5d0e24f51603e30df2f447d250b91e4d8158347e
chaabat <chaabat.02@gmail.com>
2024-10-02 12:07:47 +0100

    SB-23 SquadService

--- src/main/java/service/SquadService.java
+++ src/main/java/service/SquadService.java
@@ -1,5 +1,35 @@
 package service;
 
+import java.util.List;
+import model.Squad;
+import repository.interfaces.SquadRepository;
+import repository.implementation.SquadRepositoryImpl;
+
 public class SquadService {
 
+	private SquadRepository squadRepository ;
+	
+	public SquadService() {
+		this.squadRepository = new SquadRepositoryImpl();
+	}
+	
+	public List<Squad> getAllSquads(int page , int pageSize){
+		return squadRepository.getAllSquads(page, pageSize);
+	}
+	
+	public Squad getSquadById(long id) {
+		return squadRepository.getSquadById(id);
+	}
+	
+	public void addSquad(Squad squad) {
+		  squadRepository.addSquad(squad);
+	}
+	
+	public void updateSquad(Squad squad) {
+		 squadRepository.updateSquad(squad);
+	}
+	
+	public void deleteSquad(long id) {
+		squadRepository.deleteSquad(id);
+	}
 }
