fec9760228d929e3c8b90a70d46eca0ed3ea49c3
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-02 14:53:52 +0100

    SB-18 Task repository interface

--- src/main/java/repository/interfaces/TaskRepository.java
+++ src/main/java/repository/interfaces/TaskRepository.java
@@ -1,11 +1,20 @@
 package repository.interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 import model.Task;
 
 public interface TaskRepository {
 
+    public Optional<Task> get(long id);
+
     public List<Task> getAllTasks();
 
+    void save(Task task);
+
+    void update(Task task, String[] params);
+
+    void delete(Task task);
+    
 }
