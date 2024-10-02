443b39626060a562ff879e2916cfbb74588a23b6
WALIDSAIFI <saifiwalid21@gmail.com>
2024-10-02 11:35:07 +0100

    SB-14 Added Project table

--- src/main/java/controller/ProjectServlet.java
+++ src/main/java/controller/ProjectServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+
 public class ProjectServlet extends HttpServlet {
 
     private ProjectService projectService = new ProjectService();
